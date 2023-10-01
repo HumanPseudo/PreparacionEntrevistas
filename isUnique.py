f009369a27aa12651f46ac7ee2c08fa387f38f01
HumanPseudo <cadalagu@hotmail.com>
2023-09-30 23:30:07 -0500

    fix doc

--- isUnique.py
+++ isUnique.py
@@ -21,18 +21,39 @@ consulta en la tabla O(1) ---> O(1)
 
 """
 
-
 class IsUnique:
     def __init__(self, string):
+        """
+        Constructor de la clase IsUnique.
+
+        Parameters:
+            string (str): La cadena que se va a evaluar.
+        """
         self.string = string
 
     def is_unique(self):
+        """
+        Verifica si la cadena contiene caracteres únicos.
+
+        Returns:
+            bool: True si la cadena tiene caracteres únicos, False si no.
+        """
+        # Número máximo de caracteres posibles en el juego de caracteres ASCII
         number_of_chars = 128
+
+        # Verifica si la longitud de la cadena supera el número máximo de caracteres
         if len(self.string) > number_of_chars:
             return False
+
+        # Conjunto para almacenar caracteres únicos
         char_set = set()
+
+        # Itera a través de cada carácter en la cadena
         for char in self.string:
+            # Si el carácter ya está en el conjunto, la cadena no tiene caracteres únicos
             if char in char_set:
                 return False
             char_set.add(char)
+
+        # Si no se encontraron caracteres duplicados, la cadena tiene caracteres únicos
         return True
